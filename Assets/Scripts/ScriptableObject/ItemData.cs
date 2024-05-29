434f8eb8bfa8d98973f1d7ce42da928feea16c6d
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-05-29 15:51:03 +0900

    아이템 데이터 생성

--- Assets/Scripts/ScriptableObject/ItemData.cs
+++ Assets/Scripts/ScriptableObject/ItemData.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+public enum ItemType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Equipable,
+    Consumable,
+    Resource
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum ConsumableType
+{
+    Health,
+    Hunger
+}
+
+[Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
 }
